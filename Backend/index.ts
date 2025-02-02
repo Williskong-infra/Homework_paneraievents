eb3ba14abe335eab1c8325e36382dd83c8ef0485
williskong <willis.kong@blkchainsolutions.com>
2025-02-02 22:23:00 +0800

    update index.ts

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import { Users, Events } from './models'
 const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/check_booked', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/timeslot_and_vacancy', async (req, res) => {
+  const all_sessions = await Events.findAll()
+  
+  res.json(all_sessions)
 })
 
 app.listen(port, () => {
